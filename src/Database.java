b43a3d9b9a97c556c0dfb43d445f1167062cc34c
ASUS VivoBook M413DA <yudarangga04@gmail.com>
2022-11-05 11:07:35 +0700

    Finish

---
+++ src/Database.java
@@ -0,0 +1,21 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Database {
+    ArrayList <menu> dataMenu = new ArrayList<menu>();
+    // TODO Create List of Menu Object to Store Menu from Menu Class
+    public void inserMenu(Menu data){
+        this.dataMenu.add.(data);
+    }
+    
+
+    }
+
+    // TODO Create Method to Insert Menu to Database
+
+    // TODO Create Method to Show Menu from Database
+
+    // TODO Create Method to Search Menu from Database
+
+
+
---
+++ src/User.java
@@ -0,0 +1,28 @@
+public class User{
+    String name;
+    int nim;
+    user(String name, int nim){
+        this.name = name;
+        this.phonenumber = nim;
+    }
+
+    public void Register(){
+        System.out.println("Registrasi suskses");
+        System.out.println("name: "+name);
+        System.out.println("phone number: "+nim);
+        System.out.println("===============================");
+
+    }
+;
+
+
+}
+
+   
+
+
+    // TODO Create Attribute of User; Name and Phone Number then Create Setter
+
+    // TODO Create Method to Register User and Display User's Name and Phone Number and success message
+
+
